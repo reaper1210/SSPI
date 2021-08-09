b157df32d67d1afb36e55c875976962659e6c071
Vinesh05 <lundvinesh@gmail.com>
2021-08-09 16:23:13 +0530

    MachinesActivity Header Layout

---
+++ app/src/main/java/com/petblowmachine/sspi/MachinesActivity.kt
@@ -0,0 +1,11 @@
+package com.petblowmachine.sspi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MachinesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_machines)
+    }
+}
